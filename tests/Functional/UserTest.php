cd96ea36d9544471ec0a1489d445316894a5d4be
Renan Santos Gomes <renanonline8@gmail.com>
2018-10-24 23:05:00 -0300

    Ajuste no BaseTestCase

--- tests/Functional/UserTest.php
+++ tests/Functional/UserTest.php
@@ -11,8 +11,14 @@ class UserTest extends BaseTestCase
             'senha' => 'ad23ol12'
         );
 
-        $response = $this->runApp('POST', '/api/user/new', $bodyTest);
-
-        $this->assertEquals(200, $response->getStatusCode());
+        $resCheckUser = $this->runApp('GET', '/api/user/check_user_exist/email/' . $bodyTest['email']);
+        $result = json_decode((string)$resCheckUser->getBody());
+        
+        if (!$result->result) {
+            $response = $this->runApp('POST', '/api/user/new', $bodyTest);
+            $this->assertEquals(200, $response->getStatusCode());
+        } else {
+            $this->markTestSkipped('Usuario já existe');
+        }
     }
 }
